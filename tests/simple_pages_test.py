2f803f539870ee57858e27f4613501edbcda6fea
ChiaLinz <93101704+ChiaLinz@users.noreply.github.com>
2022-03-07 16:43:01 -0500

    Fix testing page index

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,7 +4,6 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/index")
     assert response.status_code == 308
-    assert b'<a class="nav-link" href="/index">Index</a>' in response.data
     assert b'<a class="nav-link" href="/about">About</a>' in response.data
     assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/git">Git</a>' in response.data
