19a6e224b511164103a8b310a08b9353846beec4
ChiaLinz <93101704+ChiaLinz@users.noreply.github.com>
2022-03-07 18:00:16 -0500

    Fix testing

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -12,8 +12,8 @@ def test_request_main_menu_links(client):
 
 def test_request_index(client):
     """This makes the index page"""
-    response = client.get("/index")
-    assert response.status_code == 308
+    response = client.get("/")
+    assert response.status_code == 200
     assert b"Index" in response.data
 
 def test_request_about(client):
