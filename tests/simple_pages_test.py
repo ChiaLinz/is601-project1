71404c4772bb08a042466dc91206eb726362279c
ChiaLinz <93101704+ChiaLinz@users.noreply.github.com>
2022-03-07 17:58:49 -0500

    Fix testing

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -2,8 +2,8 @@
 
 def test_request_main_menu_links(client):
     """This makes the index page"""
-    response = client.get("/index")
-    assert response.status_code == 308
+    response = client.get("/")
+    assert response.status_code == 200
     assert b'About' in response.data
     assert b'Docker' in response.data
     assert b'Git' in response.data
