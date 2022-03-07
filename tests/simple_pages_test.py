e9f27cf075c35cb1e4086f8e45eac9548c6e03c7
ChiaLinz <93101704+ChiaLinz@users.noreply.github.com>
2022-03-07 17:53:08 -0500

    Fix testing

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,11 +4,17 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/index")
     assert response.status_code == 308
-    assert b'<a class="nav-link" href="/about">About</a>' in response.data
-    assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
-    assert b'<a class="nav-link" href="/git">Git</a>' in response.data
-    assert b'<a class="nav-link" href="/cico">CI/CO</a>' in response.data
-    assert b'<a class="nav-link" href="/python">Python/Flask</a>' in response.data
+    assert b'<a href="/about"><i class="fas fa-user-circle"></i><h3>About</h3></a>' in response.data
+    assert b'<a href="/docker"><i class="fab fa-docker"></i><h3>Docker</h3></a>' in response.data
+    assert b'<a href="/git"><i class="fab fa-github"></i><h3>Git</h3></a>' in response.data
+    assert b'<a href="/cico"><i class="fas fa-sitemap" "></i><h3>CI/CO</h3></a>' in response.data
+    assert b'<a href="/python"><i class="fab fa-python"></i><h3>Python</h3></a>' in response.data
+
+def test_request_index(client):
+    """This makes the index page"""
+    response = client.get("/index")
+    assert response.status_code == 200
+    assert b"Index" in response.data
 
 def test_request_about(client):
     """This makes the index page"""
